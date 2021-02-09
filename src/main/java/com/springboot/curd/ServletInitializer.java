564ec4cd0977f32a3aa8688def6ba326ef61b962
BallaSankarReddy <ballasankarreddy@gmail.com>
2021-02-09 19:01:00 +0530

    intial level code commut

---
+++ src/main/java/com/springboot/curd/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.springboot.curd;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootCurdOptionsWithMailNotificationApplication.class);
+	}
+
+}
